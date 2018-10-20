094c3e175602ededc9acfcc129f7896f510e2c7f
Jeroen Ooms <jeroenooms@gmail.com>
2018-10-20 14:34:49 +0200

    Startup message

--- R/version.R
+++ R/version.R
@@ -11,3 +11,12 @@ git_libgit2_config <- function(){
   res$version <- as.numeric_version(res$version)
   res
 }
+
+.onLoad <- function(libname, pkgname){
+  config <- git_libgit2_config()
+  ssh <- ifelse(config$ssh, "YES", "NO")
+  https <- ifelse(config$https, "YES", "NO")
+  packageStartupMessage(sprintf(
+    "Linking to libgit2 v%s, ssh support: %s, https support: %s", 
+    as.character(config$version), ssh, https))
+}
