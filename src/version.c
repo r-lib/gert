31806b1e53269c46509170d06f74a05d77d5ed07
Jeroen Ooms <jeroenooms@gmail.com>
2018-10-20 17:06:03 +0200

    Oops

--- src/version.c
+++ src/version.c
@@ -7,7 +7,7 @@ SEXP R_libgit2_config(){
   snprintf(buffer, 99, "%d.%d.%d", LIBGIT2_VER_MAJOR, LIBGIT2_VER_MINOR, LIBGIT2_VER_REVISION);
   SEXP out = PROTECT(Rf_allocVector(VECSXP, 4));
   SET_VECTOR_ELT(out, 0, Rf_mkString(buffer));
-#if AT_LEAST_LIBGIT2(0, 99)
+#if AT_LEAST_LIBGIT2(0, 21)
   int features = git_libgit2_features();
   SET_VECTOR_ELT(out, 1, Rf_ScalarLogical(features & GIT_FEATURE_SSH));
   SET_VECTOR_ELT(out, 2, Rf_ScalarLogical(features & GIT_FEATURE_HTTPS));
