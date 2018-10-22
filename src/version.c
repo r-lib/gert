98c90b987081dcb40f2ed392739ffcf4d2a64f96
Jeroen Ooms <jeroenooms@gmail.com>
2018-10-22 16:31:41 +0200

    Distinguish between NO, and NA

--- src/version.c
+++ src/version.c
@@ -12,10 +12,10 @@ SEXP R_libgit2_config(){
   SET_VECTOR_ELT(out, 1, Rf_ScalarLogical(features & GIT_FEATURE_SSH));
   SET_VECTOR_ELT(out, 2, Rf_ScalarLogical(features & GIT_FEATURE_HTTPS));
   SET_VECTOR_ELT(out, 3, Rf_ScalarLogical(features & GIT_FEATURE_THREADS));
-#else //I think these features dit not even exit in super old libgit2
-  SET_VECTOR_ELT(out, 1, Rf_ScalarLogical(0));
-  SET_VECTOR_ELT(out, 2, Rf_ScalarLogical(0));
-  SET_VECTOR_ELT(out, 3, Rf_ScalarLogical(0));
+#else
+  SET_VECTOR_ELT(out, 1, Rf_ScalarLogical(NA_LOGICAL));
+  SET_VECTOR_ELT(out, 2, Rf_ScalarLogical(NA_LOGICAL));
+  SET_VECTOR_ELT(out, 3, Rf_ScalarLogical(NA_LOGICAL));
 #endif
   UNPROTECT(1);
   return out;
