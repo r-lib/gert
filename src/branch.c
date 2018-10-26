cb93c63e573d76d39999fec1e0c8cfe999edf4c5
Jeroen Ooms <jeroenooms@gmail.com>
2018-10-26 13:33:34 +0200

    Cleanup

--- src/branch.c
+++ src/branch.c
@@ -1,6 +1,11 @@
 #include <string.h>
 #include "utils.h"
 
+#if ! AT_LEAST_LIBGIT2(0, 21)
+#define git_checkout_options git_checkout_opts
+#define GIT_CHECKOUT_OPTIONS_INIT GIT_CHECKOUT_OPTS_INIT
+#endif
+
 SEXP R_git_create_branch(SEXP ptr, SEXP name, SEXP ref, SEXP checkout){
   git_object *obj;
   git_commit *commit = NULL;
@@ -29,11 +34,7 @@ SEXP R_git_checkout_branch(SEXP ptr, SEXP branch, SEXP force){
   bail_if(git_branch_lookup(&ref, repo, CHAR(STRING_ELT(branch, 0)), GIT_BRANCH_LOCAL), "git_branch_lookup");
 
   /* Set checkout options */
-#if AT_LEAST_LIBGIT2(0, 21)
   git_checkout_options opts = GIT_CHECKOUT_OPTIONS_INIT;
-#else
-  git_checkout_opts opts = GIT_CHECKOUT_OPTS_INIT;
-#endif
   opts.checkout_strategy = Rf_asLogical(force) ? GIT_CHECKOUT_FORCE : GIT_CHECKOUT_SAFE;
 
   git_object *obj;
@@ -46,13 +47,7 @@ SEXP R_git_checkout_branch(SEXP ptr, SEXP branch, SEXP force){
 
 SEXP R_git_checkout_ref(SEXP ptr, SEXP ref, SEXP force){
   git_repository *repo = get_git_repository(ptr);
-
-  /* Set checkout options */
-#if AT_LEAST_LIBGIT2(0, 21)
   git_checkout_options opts = GIT_CHECKOUT_OPTIONS_INIT;
-#else
-  git_checkout_opts opts = GIT_CHECKOUT_OPTS_INIT;
-#endif
   opts.checkout_strategy = Rf_asLogical(force) ? GIT_CHECKOUT_FORCE : GIT_CHECKOUT_SAFE;
 
   /* Parse the branch/tag/ref string */
