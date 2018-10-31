6e702733ab918bf8a80ba1cd3e95f1fad01998be
Jeroen Ooms <jeroenooms@gmail.com>
2018-10-31 16:14:02 +0100

    Tweaks

--- R/credential.R
+++ R/credential.R
@@ -8,6 +8,7 @@
 #' @param host hostname to authenticate with
 #' @param git path of the `git` command line program
 get_git_credential <- function(host = "github.com", git = "git"){
+  git <- find_git_cmd(git)
   input <- tempfile()
   on.exit(unlink(input))
   writeBin(charToRaw(sprintf("protocol=https\nhost=%s\n", host)), con = input)
@@ -17,5 +18,25 @@ get_git_credential <- function(host = "github.com", git = "git"){
   data <- strsplit(out, "=", fixed = TRUE)
   key <- vapply(data, `[`, character(1), 1)
   val <- vapply(data, `[`, character(1), 2)
-  as.list(structure(val, names = key))
+  c(as.list(structure(val, names = key)), list(git=git))
+}
+
+find_git_cmd <- function(git = "git"){
+  if(cmd_exists(git)){
+    return(git)
+  }
+  if(.Platform$OS.type == "windows"){
+    locations <- c("C:\\PROGRA~1\\Git\\cmd\\git.exe",
+      "C:\\Program Files\\Git\\cmd\\git.exe")
+    for(i in locations){
+      if(cmd_exists(i)){
+        return(i)
+      }
+    }
+  }
+  stop(sprintf("Could not find the '%s' command line util", git))
+}
+
+cmd_exists <- function(cmd){
+  nchar(Sys.which(cmd)) > 0
 }
