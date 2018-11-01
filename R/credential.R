637dde4554863d42e38b975975be2ff331c37ea7
Jeroen Ooms <jeroenooms@gmail.com>
2018-11-01 12:18:08 +0100

    Try with sys for now so that it can be interrupted

--- R/credential.R
+++ R/credential.R
@@ -12,13 +12,20 @@ get_git_credential <- function(host = "github.com", git = "git"){
   input <- tempfile()
   on.exit(unlink(input))
   writeBin(charToRaw(sprintf("protocol=https\nhost=%s\n", host)), con = input)
-  out <- system2(git, c("credential", "fill"), stdin = input, stdout = TRUE)
-  if(length(attr(out, "status")))
-    stop(out)
+  res <- git_credential_exec(input, git)
+  out <- strsplit(rawToChar(res$stdout), "\n", fixed = TRUE)[[1]]
   data <- strsplit(out, "=", fixed = TRUE)
   key <- vapply(data, `[`, character(1), 1)
   val <- vapply(data, `[`, character(1), 2)
   c(as.list(structure(val, names = key)), list(git=git))
+}
+
+git_credential_exec <- function(input, git){
+  if(.Platform$OS.type == "windows"){
+    sys::exec_internal("cmd", c("/C", git, "credential", "fill", "<", input))
+  } else {
+    sys::exec_internal("sh", c("-c", git, "credential", "fill", "<", input))
+  }
 }
 
 find_git_cmd <- function(git = "git"){
