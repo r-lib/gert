3c9a3da30f87d71069ff13a016c6a882570b2f4c
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-14 13:43:41 +0100

    Revert "Safer version of looking up default user"
    This reverts commit 43f38c8b10b5d4229700677805f4303a3178269d.

--- R/init.R
+++ R/init.R
@@ -5,15 +5,16 @@
   packageStartupMessage(sprintf(
     "Linking to libgit2 v%s, ssh support: %s, https support: %s",
     as.character(config$version), ssh, https))
-
-  # Show default user / email
-  tryCatch({
-    user <- git_signature_default()
-    packageStartupMessage(paste("Default user:", user))
-  }, error = function(e) {
-    packageStartupMessage("No default user configured")
+  try({
+    settings <- git_config_global()
+    name <- subset(settings, name == 'user.name')$value
+    email <- subset(settings, name == 'user.email')$value
+    if(length(name) && length(email)){
+      packageStartupMessage(sprintf("Default user: %s <%s>", name, email))
+    } else {
+      packageStartupMessage("No default user configured")
+    }
   })
-
 
   # Load tibble (if available) for pretty printing
   if(interactive() && is.null(.getNamespace('tibble'))){
