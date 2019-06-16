b61b1567e9fbe5fafb58539828eeae256165762f
Jeroen Ooms <jeroenooms@gmail.com>
2019-06-16 17:47:43 +0200

    Use new PPA

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,5 @@
-# We need https
 if(getRversion() < "3.3.0") setInternet2()
 VERSION <- commandArgs(TRUE)
-
-# Downloads libssh2 + dependencies
 if(!file.exists(sprintf("../windows/libgit2-%s/include/git2.h", VERSION))){
   download.file(sprintf("https://github.com/rwinlib/libgit2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
