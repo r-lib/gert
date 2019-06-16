82b9c2c6df463f1efe2496d4310a15ad0265d997
Jeroen Ooms <jeroenooms@gmail.com>
2019-06-16 22:30:08 +0200

    Temporary fix for Travis warning

--- src/init.c
+++ src/init.c
@@ -1,11 +1,13 @@
 #include <Rinternals.h>
 #include <git2.h>
 
-SEXP R_init_gert(){
+SEXP R_init_gert(DllInfo *info){
   #if LIBGIT2_VER_MAJOR == 0 && LIBGIT2_VER_MINOR < 22
   git_threads_init();
   #else
   git_libgit2_init();
   #endif
+  R_registerRoutines(info, NULL, NULL, NULL, NULL);
+  R_useDynamicSymbols(info, TRUE);
   return R_NilValue;
 }
