63c45b33449e0a9bc450822750b28b024c126aa3
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-05-25 12:52:26 -0700

    Documentation tweaks around links (or not) and trailing parens

--- R/repository.R
+++ R/repository.R
@@ -1,9 +1,10 @@
 #' Create or open a git repository
 #'
-#' Use [git_init()] to start a new repository or [git_clone()] to download a
+#' @description
+#' Use `git_init()` to start a new repository or `git_clone()` to download a
 #' repository from a remote.
 #'
-#' You may use [git_find()] and [git_open()] to explicitly discover and open
+#' You may use `git_find()` and `git_open()` to explicitly discover and open
 #' existing git repositories, but this is usually not needed because all gert
 #' functions also take a path argument which implicitly opens the repo.
 #'
@@ -11,8 +12,8 @@
 #' @rdname repository
 #' @name repository
 #' @family git
-#' @param path directory of the git repository. For `git_init` or `git_clone`
-#' this must be a non-existing or empty directory.
+#' @param path directory of the git repository. For [git_init()] or
+#'   [git_clone()] this must be a non-existing or empty directory.
 #' @useDynLib gert R_git_repository_init
 git_init <- function(path = '.'){
   path <- normalizePath(path.expand(path), mustWork = FALSE)
@@ -44,8 +45,8 @@ git_find <- function(path = '.'){
 
 #' @export
 #' @rdname repository
-#' @param repo a path to an existing repository, or a `git_repository` object as
-#' returned by [git_open],  [git_init] or [git_clone].
+#' @param repo a path to an existing repository, or a git repository object as
+#' returned by [git_open()], [git_init()] or [git_clone()].
 #' @useDynLib gert R_git_repository_info
 git_info <- function(repo = '.'){
   repo <- git_open(repo)
