40152fd9d0f63292817a55e18735609f3b4dbc98
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-06-02 14:33:42 -0700

    Put .onLoad() in R/init.R, with .onAttach()

--- R/aaa.R
+++
@@ -1,9 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-
-  if (identical(Sys.getenv("IN_PKGDOWN"), "true") &&
-      !global_user_is_configured()) {
-    configure_global_user()
-  }
-
-  invisible()
-}
--- R/init.R
+++ R/init.R
@@ -30,3 +30,13 @@
     }, error = function(e){})
   }
 }
+
+.onLoad <- function(libname, pkgname) {
+
+  if (identical(Sys.getenv("IN_PKGDOWN"), "true") &&
+      !global_user_is_configured()) {
+    configure_global_user()
+  }
+
+  invisible()
+}
