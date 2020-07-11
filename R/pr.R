23d52e8b4bb05c49590f5a57a1f140d3e0633b84
Jeroen Ooms <jeroenooms@gmail.com>
2020-07-11 15:58:47 +0200

    Delete the tracking feature

--- R/pr.R
+++ R/pr.R
@@ -7,25 +7,20 @@
 #'
 #' @export
 #' @rdname github
-#' @param track adds a refspec to the remote to automatically fetch updates for
-#' this PR for every `git_fetch()`.
 #' @inheritParams git_fetch
 #' @param pr number with PR to fetch or check out. Use `"*"` to fetch all
 #' pull requests.
-git_checkout_pull_request <- function(pr = 1, remote = NULL, track = FALSE, repo = '.'){
+git_checkout_pull_request <- function(pr = 1, remote = NULL, repo = '.'){
   pr <- as.character(pr)
   if(!length(remote))
     remote <- git_info(repo)$remote
   local_branch <- sprintf("pr-%s", pr)
   remote_branch <- sprintf("%s/pr/%s", remote, pr)
-  refspec <- git_fetch_pull_requests(pr = pr, remote = remote, repo = repo)
-  if(isTRUE(track) && !(refspec %in% git_remote_refspecs(name = remote, repo = repo)$refspec)){
-    git_remote_add_fetch(refspec = refspec, remote = remote, repo = repo)
-  }
   if(git_branch_exists(local_branch)){
     git_branch_checkout(local_branch, repo = repo)
     git_pull(repo = repo)
   } else {
+    git_fetch_pull_requests(pr = pr, remote = remote, repo = repo)
     git_branch_create(local_branch, remote_branch, checkout = TRUE, repo = repo)
   }
 }
