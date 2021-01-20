6dd0cc4a7a57b3392340e5ca3547cbc5fca58831
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-20 17:02:13 +0100

    Download quiet

--- get-libgit2-linux.sh
+++ get-libgit2-linux.sh
@@ -1,6 +1,6 @@
 if [ `arch` = "x86_64" ]; then
 URL="http://r-lib.github.io/gert/libgit2-1.1.0.x86_64_linux.tar.gz"
-${R_HOME}/bin/R -q -e "curl::curl_download('$URL','bundle.tar.gz',quiet=FALSE)"
+${R_HOME}/bin/R -q -e "curl::curl_download('$URL','bundle.tar.gz')"
 tar xzf bundle.tar.gz && rm -f bundle.tar.gz
 PKG_CFLAGS="-I${PWD}/libgit2/include"
 PKG_LIBS="-L${PWD}/libgit2/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl -lpcre -lz"
