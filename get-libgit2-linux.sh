543572597c7f1818341a009b7263a94b120cfe03
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-25 18:50:08 +0100

    check for musl c

--- get-libgit2-linux.sh
+++ get-libgit2-linux.sh
@@ -1,11 +1,12 @@
-if [ `arch` = "x86_64" ]; then
+IS_MUSL=$(ldd --version 2>&1 | grep musl)
+if [ $? -eq 0 ] && [ "$IS_MUSL" ]; then
+echo "Do not use static libgit2 on MUSL C"
+else
 URL="http://r-lib.github.io/gert/libgit2-1.1.0.x86_64_linux.tar.gz"
 ${R_HOME}/bin/R -q -e "curl::curl_download('$URL','bundle.tar.gz')"
 tar xzf bundle.tar.gz && rm -f bundle.tar.gz
 PKG_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2/include"
 PKG_LIBS="-L${PWD}/libgit2/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl -lpcre -lz"
 HAVE_STATIC_LIBGIT2=TRUE
-else
-"DOWNLOAD_STATIC_LIBGIT2 not supported on `arch`"
-exit 1
 fi
+
