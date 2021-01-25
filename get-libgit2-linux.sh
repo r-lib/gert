4c96f977d2ff7b0e0437862fa493e5c761b05367
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-25 17:40:45 +0100

    add flag

--- get-libgit2-linux.sh
+++ get-libgit2-linux.sh
@@ -4,6 +4,7 @@ ${R_HOME}/bin/R -q -e "curl::curl_download('$URL','bundle.tar.gz')"
 tar xzf bundle.tar.gz && rm -f bundle.tar.gz
 PKG_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2/include"
 PKG_LIBS="-L${PWD}/libgit2/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl -lpcre -lz"
+HAVE_STATIC_LIBGIT2=TRUE
 else
 "DOWNLOAD_STATIC_LIBGIT2 not supported on `arch`"
 exit 1
