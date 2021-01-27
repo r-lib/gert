3822b6fe7da5054779a04e79499c7cce118c1a08
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-27 15:29:43 +0100

    no echo

--- get-libgit2-linux.sh
+++ get-libgit2-linux.sh
@@ -3,7 +3,7 @@ if [ $? -eq 0 ] && [ "$IS_MUSL" ]; then
 echo "Do not use static libgit2 on MUSL C"
 else
 URL="http://r-lib.github.io/gert/libgit2-1.1.0.x86_64_linux.tar.gz"
-${R_HOME}/bin/R -q -e "curl::curl_download('$URL','bundle.tar.gz')"
+${R_HOME}/bin/R --no-echo -q -e "curl::curl_download('$URL','bundle.tar.gz')"
 tar xzf bundle.tar.gz && rm -f bundle.tar.gz
 PKG_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2/include"
 PKG_LIBS="-L${PWD}/libgit2/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl -lpcre -lz"
