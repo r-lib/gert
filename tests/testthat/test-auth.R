49de0e562763e7803f09e0bed1a97bce0d1b7449
Jeroen Ooms <jeroenooms@gmail.com>
2022-03-29 21:28:58 +0200

    Switch to new testprivate repo + ecdsa key

--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -3,7 +3,7 @@ test_that("public ssh remotes with random key", {
   skip_if_offline('github.com')
   remote <- 'git@github.com:jeroen/webp.git'
   target <- file.path(tempdir(), basename(remote))
-  repo <- git_clone(remote, path = target, ssh_key = 'key.pem', password = 'testingjerry')
+  repo <- git_clone(remote, path = target, ssh_key = 'ssh.key', password = 'testingjerry')
   expect_true(file.exists(file.path(target, 'DESCRIPTION')))
 })
 
@@ -12,11 +12,11 @@ isOldWindows <- Sys.info()[["sysname"]] == "Windows" && grepl('Windows Server 20
 # Even for public repos, Github only allows keys that it knows.
 test_that("private ssh remotes with key", {
   skip_if_offline('github.com')
-  remote <- 'git@github.com:ropensci/testprivate.git'
+  remote <- 'git@github.com:jeroenooms/testprivate.git'
   target <- file.path(tempdir(), basename(remote))
 
   # Also test password as a callback function
-  repo <- git_clone(remote, path = target, ssh_key = 'key.pem', password = function(...){ 'testingjerry'})
+  repo <- git_clone(remote, path = target, ssh_key = 'ssh.key', password = function(...){ 'testingjerry'})
   expect_true(file.exists(file.path(target, 'hello')))
 
   # Test errors
@@ -24,7 +24,7 @@ test_that("private ssh remotes with key", {
   expect_error(git_clone(remote, path = tempfile(), ssh_key = 'pat.bin'), 'load key', class = 'GIT_EAUTH')
 
   # Test ls-remote auth
-  git_remote_ls(repo = target, ssh_key = 'key.pem', password = function(...){ 'testingjerry'})
+  git_remote_ls(repo = target, ssh_key = 'ssh.key', password = function(...){ 'testingjerry'})
 })
 
 # Access token for dummy account with minimal rights
@@ -37,13 +37,13 @@ test_that("HTTP user/pass auth", {
   enc <- readBin('pat.bin', raw(), 1e3)
   dec <- openssl::rsa_decrypt(enc, 'key.pem', password = 'testingjerry')
   target2 <- file.path(tempdir(), 'testprivate2')
-  repo <- git_clone('https://testingjerry@github.com/ropensci/testprivate',
+  repo <- git_clone('https://testingjerry@github.com/jeroenooms/testprivate',
                     path = target2, password = rawToChar(dec))
   expect_true(file.exists(file.path(target2, 'hello')))
 
   # Test with password in URL
   target3 <- file.path(tempdir(), 'testprivate3')
-  repo <- git_clone(sprintf('https://testingjerry:%s@github.com/ropensci/testprivate',
+  repo <- git_clone(sprintf('https://testingjerry:%s@github.com/jeroenooms/testprivate',
                             rawToChar(dec)), path = target3)
   expect_true(file.exists(file.path(target3, 'hello')))
 
@@ -51,7 +51,7 @@ test_that("HTTP user/pass auth", {
   # This seems to fail on WinBuilder due to some syscall
   if(!isTRUE(isOldWindows)){
     # Test that repo is private
-    expect_error(git_clone('https://github.com/ropensci/testprivate',
+    expect_error(git_clone('https://github.com/jeroenooms/testprivate',
                            password = "bla", path = tempfile()), 'Authentication', class = 'GIT_EAUTH')
   }
 
@@ -59,7 +59,7 @@ test_that("HTTP user/pass auth", {
   Sys.setenv(GITHUB_PAT = rawToChar(dec))
   on.exit(Sys.unsetenv("GITHUB_PAT"))
   target4 <- file.path(tempdir(), 'testprivate4')
-  repo <- git_clone('https://github.com/ropensci/testprivate', path = target4)
+  repo <- git_clone('https://github.com/jeroenooms/testprivate', path = target4)
   expect_true(file.exists(file.path(target4, 'hello')))
   heads <- git_remote_ls(repo = repo)
   expect_is(heads, 'data.frame')
@@ -67,7 +67,7 @@ test_that("HTTP user/pass auth", {
 
   # Try with user in URL
   target5 <- file.path(tempdir(), 'testprivate5')
-  repo <- git_clone('https://nobody@github.com/ropensci/testprivate', path = target5)
+  repo <- git_clone('https://nobody@github.com/jeroenooms/testprivate', path = target5)
   expect_true(file.exists(file.path(target5, 'hello')))
   heads <- git_remote_ls(repo = repo)
   expect_is(heads, 'data.frame')
