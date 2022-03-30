d9f65d57ca101ad38c101b581f6fac181366246d
Jeroen Ooms <jeroenooms@gmail.com>
2022-03-30 16:14:33 +0200

    Silence download some more

--- get-libgit2-linux.sh
+++ get-libgit2-linux.sh
@@ -7,7 +7,7 @@ OLDOPENSSL=$("${R_HOME}/bin/Rscript" -e 'cat(openssl::openssl_config()$version)'
 if [ $? -eq 0 ] && [ "$OLDOPENSSL" ]; then
 URL="https://r-lib.github.io/gert/libgit2-1.1.0.x86_64_legacy-linux.tar.gz"
 fi
-"${R_HOME}/bin/R" -q -e "curl::curl_download('$URL','bundle.tar.gz')"
+"${R_HOME}/bin/R" -s -e "curl::curl_download('$URL','bundle.tar.gz')"
 tar xzf bundle.tar.gz && rm -f bundle.tar.gz
 PKG_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2/include"
 PKG_LIBS="-L${PWD}/libgit2/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl -lpcre -lz"
