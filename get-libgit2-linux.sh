50f149ca62280397d08818650c4d29dd07fbc631
Jeroen Ooms <jeroenooms@gmail.com>
2022-08-17 14:38:49 +0200

    update script for new bottle

--- get-libgit2-linux.sh
+++ get-libgit2-linux.sh
@@ -2,14 +2,17 @@ IS_MUSL=$(ldd --version 2>&1 | grep musl)
 if [ $? -eq 0 ] && [ "$IS_MUSL" ]; then
 echo "Do not use static libgit2 on MUSL C"
 else
-URL="https://r-lib.github.io/gert/libgit2-1.1.0.x86_64_linux.tar.gz"
 OLDOPENSSL=$("${R_HOME}/bin/Rscript" -e 'cat(openssl::openssl_config()$version)' | grep "OpenSSL 1.0")
 if [ $? -eq 0 ] && [ "$OLDOPENSSL" ]; then
 URL="https://r-lib.github.io/gert/libgit2-1.1.0.x86_64_legacy-linux.tar.gz"
+PKG_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2/include"
+PKG_LIBS="-L${PWD}/libgit2/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl -lpcre -lz"
+else
+URL="https://r-lib.github.io/gert/libgit2-1.5.0-x86_64_linux.tar.gz"
+PKG_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2-1.5.0-x86_64_linux/include"
+PKG_LIBS="-L${PWD}/libgit2-1.5.0-x86_64_linux/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl"
 fi
 "${R_HOME}/bin/R" -s -e "curl::curl_download('$URL','bundle.tar.gz')"
 tar xzf bundle.tar.gz && rm -f bundle.tar.gz
-PKG_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2/include"
-PKG_LIBS="-L${PWD}/libgit2/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl -lpcre -lz"
 HAVE_STATIC_LIBGIT2=TRUE
 fi
