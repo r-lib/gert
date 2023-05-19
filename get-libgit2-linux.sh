39a29aa9815b5d94f1c37d36cc6f98e7a22d2f9d
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-19 11:11:06 +0200

    prevent linking conflicts

--- get-libgit2-linux.sh
+++ get-libgit2-linux.sh
@@ -6,7 +6,8 @@ OLDOPENSSL=$("${R_HOME}/bin/Rscript" -e 'cat(openssl::openssl_config()$version)'
 if [ $? -eq 0 ] && [ "$OLDOPENSSL" ]; then
 URL="https://r-lib.github.io/gert/libgit2-1.1.0.x86_64_legacy-linux.tar.gz"
 TMP_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2/include"
-TMP_LIBS="-L${PWD}/libgit2/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl -lpcre -lz"
+TMP_DIR="${PWD}/libgit2/lib"
+TMP_LIBS="-L${TMP_DIR} -lstatgit2 -lrt -lpthread -lstatssh2 -lstatssl -lstatcrypto -ldl -lpcre -lz"
 else
 OPENSSL3=$("${R_HOME}/bin/Rscript" -e 'cat(openssl::openssl_config()$version)' | grep "OpenSSL 3")
 if [ $? -eq 0 ] && [ "$OPENSSL3" ]; then
@@ -16,10 +17,15 @@ else
 URL="https://autobrew.github.io/archive/x86_64_linux/libgit2-1.4.2-x86_64_linux.tar.gz"
 fi
 TMP_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2-1.4.2-x86_64_linux/include"
-TMP_LIBS="-L${PWD}/libgit2-1.4.2-x86_64_linux/lib -lgit2 -lrt -lpthread -lssh2 -lssl -lcrypto -ldl"
+TMP_DIR="${PWD}/libgit2-1.4.2-x86_64_linux/lib"
+TMP_LIBS="-L${TMP_DIR} -lstatgit2 -lrt -lpthread -lstatssh2 -lstatssl -lstatcrypto -ldl"
 fi
 if "${R_HOME}/bin/R" -s -e "curl::curl_download('$URL','bundle.tar.gz')"; then
   tar xzf bundle.tar.gz && rm -f bundle.tar.gz
+  cp $TMP_DIR/libgit2.a $TMP_DIR/libstatgit2.a
+  cp $TMP_DIR/libssh2.a $TMP_DIR/libstatssh2.a
+  cp $TMP_DIR/libssl.a $TMP_DIR/libstatssl.a
+  cp $TMP_DIR/libcrypto.a $TMP_DIR/libstatcrypto.a
   HAVE_STATIC_LIBGIT2=TRUE
   PKG_CFLAGS="$TMP_CFLAGS"
   PKG_LIBS="$TMP_LIBS"
