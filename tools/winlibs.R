952e5420fcd2960f75ad915d169ccff2922c7699
Jeroen Ooms <jeroenooms@gmail.com>
2023-09-26 12:09:03 +0200

    Bump version for release

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,6 +2,8 @@ if(!file.exists("../windows/libgit2/include/git2.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
     "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.1/libgit2-1.7.1-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.1/libgit2-1.7.1-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.2") {
     "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.1/libgit2-1.7.1-ucrt-x86_64.tar.xz"
   } else {
