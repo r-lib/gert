89f35aac55c49beeddfd7e730d50a3cdcb31c0f8
Jeroen Ooms <jeroenooms@gmail.com>
2024-10-21 20:46:28 +0200

    Update libgit2 on Windows and MacOS

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,11 +1,11 @@
 if(!file.exists("../windows/libgit2/include/git2.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.1/libgit2-1.7.1-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.2/libgit2-1.7.2-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.1/libgit2-1.7.1-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.2/libgit2-1.7.2-clang-x86_64.tar.xz"
   } else if(getRversion() >= "4.2") {
-    "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.1/libgit2-1.7.1-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/libgit2-1.7.2/libgit2-1.7.2-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/libgit2/archive/v1.7.1.tar.gz"
   }
