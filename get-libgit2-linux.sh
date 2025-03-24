4a4347ad3440f95900edf02247134809643f8f78
Jeroen Ooms <jeroenooms@gmail.com>
2025-03-25 00:03:42 +0100

    Move to .deps

--- get-libgit2-linux.sh
+++ get-libgit2-linux.sh
@@ -5,9 +5,7 @@ else
 OLDOPENSSL=$("${R_HOME}/bin/Rscript" -e 'cat(openssl::openssl_config()$version)' | grep "OpenSSL 1.0")
 if [ $? -eq 0 ] && [ "$OLDOPENSSL" ]; then
 URL="https://r-lib.github.io/gert/libgit2-1.1.0.x86_64_legacy-linux.tar.gz"
-TMP_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2/include"
-TMP_DIR="${PWD}/libgit2/lib"
-TMP_LIBS="-L${TMP_DIR} -lstatgit2 -lrt -lpthread -lstatssh2 -lstatssl -lstatcrypto -ldl -lpcre -lz"
+TMP_LIBS="-lstatgit2 -lrt -lpthread -lstatssh2 -lstatssl -lstatcrypto -ldl -lpcre -lz"
 else
 OPENSSL3=$("${R_HOME}/bin/Rscript" -e 'cat(openssl::openssl_config()$version)' | grep "OpenSSL 3")
 if [ $? -eq 0 ] && [ "$OPENSSL3" ]; then
@@ -16,18 +14,19 @@ echo "Found OpenSSL3"
 else
 URL="https://autobrew.github.io/archive/x86_64_linux/libgit2-1.4.2-x86_64_linux.tar.gz"
 fi
-TMP_CFLAGS="-DSTATIC_LIBGIT2 -I${PWD}/libgit2-1.4.2-x86_64_linux/include"
-TMP_DIR="${PWD}/libgit2-1.4.2-x86_64_linux/lib"
-TMP_LIBS="-L${TMP_DIR} -lstatgit2 -lrt -lpthread -lstatssh2 -lstatssl -lstatcrypto -ldl"
+TMP_LIBS="-lstatgit2 -lrt -lpthread -lstatssh2 -lstatssl -lstatcrypto -ldl"
 fi
 if "${R_HOME}/bin/R" -s -e "curl::curl_download('$URL','bundle.tar.gz')"; then
-  tar xzf bundle.tar.gz && rm -f bundle.tar.gz
-  cp $TMP_DIR/libgit2.a $TMP_DIR/libstatgit2.a
-  cp $TMP_DIR/libssh2.a $TMP_DIR/libstatssh2.a
-  cp $TMP_DIR/libssl.a $TMP_DIR/libstatssl.a
-  cp $TMP_DIR/libcrypto.a $TMP_DIR/libstatcrypto.a
+  DEPS="$PWD/.deps"
+  LIBDIR="$DEPS/lib"
+  mkdir -p $DEPS
+  tar xzf bundle.tar.gz --strip 1 -C $DEPS
+  mv $LIBDIR/libgit2.a $LIBDIR/libstatgit2.a
+  mv $LIBDIR/libssh2.a $LIBDIR/libstatssh2.a
+  mv $LIBDIR/libssl.a $LIBDIR/libstatssl.a
+  mv $LIBDIR/libcrypto.a $LIBDIR/libstatcrypto.a
+  PKG_CFLAGS="-I$DEPS/include -DSTATIC_LIBGIT2"
+  PKG_LIBS="-L$LIBDIR $TMP_LIBS"
   HAVE_STATIC_LIBGIT2=TRUE
-  PKG_CFLAGS="$TMP_CFLAGS"
-  PKG_LIBS="$TMP_LIBS"
 fi
 fi
